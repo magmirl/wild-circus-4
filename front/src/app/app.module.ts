4751c51ebaec13e46c5f8c7a1a47df6cd7c871c1
magmirl <mirloupm@gmail.com>
2019-07-16 19:28:20 +0200

    create components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PerformancesComponent } from './performances/performances.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PricesComponent } from './prices/prices.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PerformancesComponent,
+    AboutUsComponent,
+    PricesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
